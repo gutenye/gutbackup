d6f6d0f17cfbfe551b2f4b799cb790255e8bb442
Guten <ywzhaifei@gmail.com>
2014-01-06 12:37:45 +0800

    fix bugs to make it work

--- libexec/lib.sh
+++ libexec/lib.sh
@@ -1,18 +1,13 @@
-before() { :; }
-after() { :; }
-
-run_as_root() {
-	if [[ `id -u` -ne 0 ]]; then 
-		echo "oldtime: you cannot perform this operation unless you are root." >&2
-		exit 1
-	fi
+absolutename() {
+	readlink -f "$1"
 }
 
-check_mountpoint() {
-	if ! mountpoint -q "$1"; then 
-		echo "oldtime: \`$1' is unmounted." >&2
-		exit 1
-	fi
+filename() {
+	base=$(basename "$1")
+	echo ${base%.*}
 }
 
-# vim: ft=sh
+extname() {
+	base=$(basename "$1")
+	[[ "$base" =~ \. ]] && echo "${base##*.}" || echo ""
+}
