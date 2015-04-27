19bd100151056c7ead243ee4cc2b07d42367d4d6
Guten Ye <ywzhaifei@gmail.com>
2015-04-27 09:02:07 +0000

    use data/udev/work as working directory

--- libexec/helpers.sh
+++ libexec/helpers.sh
@@ -2,8 +2,8 @@ run_as_root() {
   [[ $(id -u) -ne 0 ]] && error_exit "gutbackup: you cannot perform this operation unless you are root."
 }
 
-check_mountpoint() {
-	[[ ! mountpoint -q "$1" ]] && error_exit "gutbackup: \`$1' is unmounted."
-}
+#check_mountpoint() {
+  #[[ ! mountpoint -q "$1" ]] && error_exit "gutbackup: \`$1' is unmounted."
+#}
 
 # vim: ft=sh
